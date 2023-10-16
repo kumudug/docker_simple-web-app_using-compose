8ea80555918e50dda74175a1bb22f1d48e990326
kumudug <kumudug1@gmail.com>
2023-10-16 07:32:01 -0700

    * Fixed the error    - `Mongoose.prototype.connect() no longer accepts a callback`

--- backend/app.js
+++ backend/app.js
@@ -87,15 +87,15 @@ mongoose.connect(
   'mongodb://localhost:27017/course-goals',
   {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-  },
-  (err) => {
-    if (err) {
-      console.error('FAILED TO CONNECT TO MONGODB');
-      console.error(err);
-    } else {
+    useUnifiedTopology: true
+  })
+  .then(
+    () => {
       console.log('CONNECTED TO MONGODB');
       app.listen(80);
     }
-  }
-);
+  )
+  .catch((err) => {
+    console.error('FAILED TO CONNECT TO MONGODB');
+    console.error(err);
+  });
